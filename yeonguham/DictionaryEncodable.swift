4b81c1826546a5da6f0036fd47a499a7088f5979
Subeen Park <psb021414@gmail.com>
2021-08-18 19:26:04 +0900

    feature: force lightmode

--- yeonguham/DictionaryEncodable.swift
+++ yeonguham/DictionaryEncodable.swift
@@ -4,5 +4,21 @@
 //
 //  Created by Subeen Park on 2021/08/16.
 //
+// 
 
 import Foundation
+
+
+protocol DictionaryEncodable: Encodable {}
+
+extension DictionaryEncodable {
+    func dictionary() -> [String: Any]? {
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .millisecondsSince1970
+        guard let json = try? encoder.encode(self),
+            let dict = try? JSONSerialization.jsonObject(with: json, options: []) as? [String: Any] else {
+                return nil
+        }
+        return dict
+    }
+}
