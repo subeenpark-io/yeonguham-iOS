242e6adb16af9fa03cf86f0be17dfe6ba2e96eea
Subeen Park <psb021414@gmail.com>
2021-08-19 13:10:25 +0900

    feat: FieldCollectionView

--- yeonguham/Resources/Constant/Const.swift
+++ yeonguham/Resources/Constant/Const.swift
@@ -12,4 +12,8 @@ import UIKit
 struct Const {
     // Extension에 자세한 상수 기입
     static let shared = Const()
+    static let tagList: [String] = ["의약학", "생명과학", "전자공학", "컴퓨터공학", "기계공학", "환경공학", "식품영양학", "건축학",
+    "경제학", "경영학", "심리학", "언론정보학", "인류학", "언어학", "의류학", "교육학", "예체능"]
+    static let imageList: [String] = ["iconPharmacy", "iconBiology", "iconElectronics", "iconComputer", "iconMechanical",
+    "iconEnvironmental", "iconNutrition", "iconArchitecture", "iconEconomics", "iconBusiness", "iconPsychology", "iconPsychology", "iconAnthropology", "iconLinguistic", "iconClothing", "iconEducation", "iconEntertainment"]
 }
--- yeonguham/Sources/Cell/FieldTableViewCell.swift
+++ yeonguham/Sources/Cell/FieldTableViewCell.swift
@@ -6,18 +6,45 @@
 //
 
 import UIKit
+import Then
 
-class FieldTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+class FieldCollectionViewCell: UICollectionViewCell {
+    
+    let image: UIImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    let label: UILabel = UILabel().then {
+        $0.font = .typo7
+        $0.textColor = .gray5
+    }
 
-        // Configure the view for the selected state
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        contentView.addSubviews(image, label)
+        
+        image.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(32)
+            $0.top.equalToSuperview().offset(3)
+        }
+        
+        label.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(image.snp.bottom)
+            $0.height.equalTo(18)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func setData(title: String, imageStr: String) {
+        label.text = title
+        image.image = UIImage(named: imageStr)
     }
 
 }
